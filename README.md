eb2f907406746652d50aeca572e98bdfe85cb97c
Flongy <craftbymax@gmail.com>
2022-02-20 13:42:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Видео
+## Часть 1
+[![Видео](https://img.youtube.com/vi/0iBVH2Y9j-s/maxresdefault.jpg)](https://youtu.be/0iBVH2Y9j-s)
+
+## Часть 2
+[![Видео](https://img.youtube.com/vi/4Wiqf9hoRmA/maxresdefault.jpg)](https://youtu.be/4Wiqf9hoRmA)
+
+## Плейлист
+[![Видео](https://img.youtube.com/vi/0iBVH2Y9j-s/maxresdefault.jpg)](https://www.youtube.com/watch?v=0iBVH2Y9j-s&list=PL9zxc4HyFoESfSwiNAGttz4pXgTjBa3Ra)
+
+
+# Описание
+Пример простенького HTTP сервера. Его текущие возможности:
+- Статическая страница
+- Эхо-страница
+- Выдача страницы из файла
+
+
+# Полезные ссылки
+- Модуль `http` стандартной библиотеки `Python`: https://docs.python.org/3/library/http.html
+- HTTP сервер: https://docs.python.org/3/library/http.server.html
+- Статусы HTTP и их описания: https://developer.mozilla.org/ru/docs/Web/HTTP/Status
+- Заголовки HTTP (на английском): https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
---
+++ main.py
@@ -0,0 +1,58 @@
+""" Кастомный HTTP сервер на Python """
+from http import HTTPStatus
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class OurHandler(BaseHTTPRequestHandler):
+    """ Пример обработчика HTTP запросов """
+
+    def not_found(self):
+        """ Страница для ненайденных ресурсов (страниц) """
+        self.send_response(HTTPStatus.NOT_FOUND)
+        self.send_header("Content-Type", "text/html; charset=UTF-8")
+        self.end_headers()
+
+        self.wfile.write("<h1>404 Not Found!</h1>".encode("utf-8"))
+
+    def simple_page(self):
+        """ Статическая страница с фиксированным HTML """
+        self.send_response(HTTPStatus.OK)
+        self.send_header("Content-Type", "text/html; charset=UTF-8")
+        self.end_headers()
+
+        self.wfile.write("<h1>Subscribe to @FlongyDev</h1>".encode("utf-8"))
+
+    def echo_page(self):
+        """ Эхо-страница: возвращает строку запроса от клиента """
+        self.send_response(HTTPStatus.OK)
+        self.send_header("Content-Type", "text/plain; charset=UTF-8")
+        self.end_headers()
+
+        ip, _ = self.client_address
+        msg = f"{self.requestline}\nIP address: {ip}"
+        self.wfile.write(msg.encode("utf-8"))
+
+    def index_page(self):
+        """ Страница из файла index.html """
+        self.send_response(HTTPStatus.OK)
+        self.send_header("Content-Type", "text/html; charset=UTF-8")
+        self.end_headers()
+
+        with open("index.html", 'rb') as f:
+            self.wfile.write(f.read())
+
+    def do_GET(self):
+        """ Обработка GET запросов к серверу """
+        if self.path == "/":
+            self.simple_page()
+        elif self.path.startswith("/echo"):
+            self.echo_page()
+        elif self.path.startswith("/index"):
+            self.index_page()
+        else:
+            self.not_found()
+
+
+if __name__ == "__main__":
+    with HTTPServer(('', 8000), OurHandler) as server:
+        server.serve_forever()
